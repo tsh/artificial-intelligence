29be1dfdc3c7bcb080cad19c74c4bdac6c97792f
Anton Nikulin <dr.tallin@gmail.com>
2020-01-28 21:53:25 +0200

    initial open book generation

---
+++ Projects/3_Adversarial Search/generate_book.py
@@ -0,0 +1,36 @@
+import random
+from isolation import Isolation
+from collections import defaultdict, Counter
+
+
+NUMBER_OF_SIMULATION = 100
+MAX_DEPTH = 4
+
+def generate_book():
+    book = defaultdict(Counter)
+    for _ in range(NUMBER_OF_SIMULATION):
+        initial_state = Isolation()
+        build_tree(initial_state, MAX_DEPTH, book)
+    return {k: max(v, key=v.get) for k, v in book.items()}
+
+
+def build_tree(state, depth, book):
+    if depth == 0 or state.terminal_test():
+        return simulate(state)
+    action = random.choice(state.actions())
+    new_state = state.result(action)
+    reward = build_tree(new_state, depth-1, book)
+    book[state][action] += reward
+    return -reward
+
+def simulate(state):
+    player = state.player()
+    while not state.terminal_test():
+        action = random.choice(state.actions())
+        state = state.result(action)
+    return -1 if state.utility(player) < 0 else 1
+
+
+book = generate_book()
+
+
